87189a43b282a5189e9c4415728115c4a859cf10
ThisIsJustAGuy <bbalint0404@gmail.com>
2024-05-27 12:50:58 +0200

    help

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,10 @@ Route::get('/', HomeController::class)->name('home');
 
 Route::resource('products', ProductsController::class);
 Route::resource('orders', OrderController::class);
+
+//php artisan make:model BookCategory -crR
+
+//composer require laravel/ui
+//php artisan ui bootstrap --auth
+//npm run build
+//chown -R www-data:www-data /var/www/storage
